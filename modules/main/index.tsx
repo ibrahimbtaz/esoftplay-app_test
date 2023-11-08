c8ae983d109feb047a98159e808105845f3833a2
ibrahimbtaz <ibrahimbtaz@gmail.com>
2023-11-08 23:25:24 +0700

    try react native

--- modules/main/index.tsx
+++ modules/main/index.tsx
@@ -3,17 +3,27 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-
-export interface MainIndexArgs {
-	
-}
-export interface MainIndexProps {
-	
-}
+export interface MainIndexArgs {}
+export interface MainIndexProps {}
 export default function m(props: MainIndexProps): any {
-	return (
-		<View style={{ flex: 1, backgroundColor:"green"}}>
-			<Text>irfie</Text>
-		</View>
-	)
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'green',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 14,
+          textAlign: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Halo Saya Surya
+      </Text>
+    </View>
+  );
 }
